c0367a9ec342225ed185b6730e7de1e80fd8dd47
tati <tatianavega89@gmail.com>
2020-08-10 18:36:42 -0300

    Updates

---
+++ precache-manifest.5cc92559b6c6e8f7af94d91831fe06ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621afa665eb38a48203011951e473daf",
+    "url": "/demos/index.html"
+  },
+  {
+    "revision": "985f74b3a798da2bce93",
+    "url": "/demos/static/css/main.8bac5ba7.chunk.css"
+  },
+  {
+    "revision": "929058c86cf61b88f00e",
+    "url": "/demos/static/js/2.923867a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demos/static/js/2.923867a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985f74b3a798da2bce93",
+    "url": "/demos/static/js/main.02d6920b.chunk.js"
+  },
+  {
+    "revision": "a9a0ac4f226cf803d131",
+    "url": "/demos/static/js/runtime-main.186f251c.js"
+  }
+]);
